133fa256d122dc28163e0b3f951b5a70316a0a23
DanielDavalos93 <93288354+DanielDavalos93@users.noreply.github.com>
2023-04-21 21:45:59 -0400

    Create volumen.sh

---
+++ .local/bin/volumen.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+# You can call this script like this:
+# $ ./volumeControl.sh up
+# $ ./volumeControl.sh down
+# $ ./volumeControl.sh mute
+
+# Script modified from these wonderful people:
+# https://github.com/dastorm/volume-notification-dunst/blob/master/volume.sh
+# https://gist.github.com/sebastiencs/5d7227f388d93374cebdf72e783fbd6a
+
+function get_volume {
+  amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
+}
+
+function is_mute {
+  amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
+}
+
+iconMuted = "/usr/share/icons/Zafiro-Nord-Black/panel/22/audio-volume-muted.svg"
+#iconSound = "/usr/share/icons/Zafiro-Nord-Black/panel/22/audio-volume-medium.svg"
+
+function send_notification {
+  iconSound="audio-volume-high"
+  iconMuted="audio-volume-muted"
+  if is_mute ; then
+    dunstify -i $iconMuted -r 2593 -u normal "mute"
+  else
+    volume=$(get_volume)
+    # Make the bar with the special character ─ (it's not dash -)
+    # https://en.wikipedia.org/wiki/Box-drawing_character
+    bar=$(seq --separator="─" 0 "$((volume / 5))" | sed 's/[0-9]//g')
+    # Send the notification
+    dunstify -i $iconSound -r 2593 -u normal "    $bar"
+  fi
+}
+
+case $1 in
+  up)
+    # set the volume on (if it was muted)
+    amixer -q set Master on > /dev/null
+    # up the volume (+ 5%)
+    amixer -c 0 sset Master 5+ unmute > /dev/null
+    send_notification
+    ;;
+  down)
+    amixer -q set Master on > /dev/null
+    amixer -c 0 sset Master 5- > /dev/null
+    send_notification
+    ;;
+  mute)
+    # toggle mute
+    amixer -q set Master toggle > /dev/null
+    send_notification
+    ;;
+esac
